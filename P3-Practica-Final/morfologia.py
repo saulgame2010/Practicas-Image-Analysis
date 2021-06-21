8c38da59c27fbf134b66e5f6740cfab406ff4035
LuisJorgeGuadarrama <82246910+LuisJorgeGuadarrama@users.noreply.github.com>
2021-06-21 14:31:28 -0500

    Dilatacion & Erosion
    Funcionando los dos

--- P3-Practica-Final/morfologia.py
+++ P3-Practica-Final/morfologia.py
@@ -19,3 +19,15 @@ class Morfologia:
         kernel = np.ones((n, n), np.uint8)
         imgRes = cv.morphologyEx(img, cv.MORPH_BLACKHAT, kernel)
         self.mostrarImagen("Black hat", img, imgRes, "blackH", ruta)
+
+    def Erosion(self, ruta, n):
+        img = cv.imread("./img/" + ruta, cv.IMREAD_GRAYSCALE)
+        kernel = np.ones((n, n), np.uint8)
+        imgRes = cv.erode(img,kernel,iterations = 1)
+        self.mostrarImagen("Erosion", img, imgRes, "Eros", ruta)
+
+    def Dilatacion(self, ruta, n):
+        img = cv.imread("./img/" + ruta, cv.IMREAD_GRAYSCALE)
+        kernel = np.ones((n, n), np.uint8)
+        imgRes = cv.dilate(img,kernel,iterations = 1)
+        self.mostrarImagen("Dilatacion", img, imgRes, "Dilat", ruta)
